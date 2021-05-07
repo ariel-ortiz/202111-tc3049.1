3dd47fc00b7bb37160e680da09b353c6e3c015ca
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-05-07 23:45:49 +0000

    Adds client code that GETs the full quotations collection

--- 5. AWS Lambda/quotations/client.rb
+++ 5. AWS Lambda/quotations/client.rb
@@ -1,12 +1,12 @@
 require 'json'
 require 'faraday'
 
-# Replace the following URL with the corresponding API gateway endpoint 
-URL = 'https://something.amazonaws.com/default/quotations'
+# Replace the following URL with the corresponding API gateway endpoint
+URL = 'https://7u0ehap743.execute-api.us-west-2.amazonaws.com/default/quotations'
 
 connection = Faraday.new(url: URL)
 response = connection.get do |request|
-  request.params['id'] = 1
+  request.params['id'] = 5
 end
 
 if response.success?
@@ -15,4 +15,13 @@ if response.success?
   puts "- ID: #{data['id']}"
   puts "- Author: #{data['author']}"
   puts "- Excerpt: #{data['excerpt']}"
+end
+
+puts
+
+connection = Faraday.new(url: URL)
+response = connection.get
+if response.success?
+  data = JSON.parse(response.body)
+  p data
 end
